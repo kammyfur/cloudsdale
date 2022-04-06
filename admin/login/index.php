ad6045f0184bc0ca2e98933a748f03e7c726e6af
Minteck <contact@minteck.org>
2022-04-06 21:58:17 +0200

    Fix OAuth2 issues

--- admin/login/index.php
+++ admin/login/index.php
@@ -1,4 +1,11 @@
 <?php
 
-header("Location: https://github.com/login/oauth/authorize?client_id=" . json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/data/github.json"), true)["id"] . "&redirect_uri=http://$_SERVER[HTTP_HOST]/admin/callback/&allow_signups=false&scope=read:user");
+function isSecure() {
+    return
+        (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
+        || $_SERVER['SERVER_PORT'] == 443;
+}
+
+
+header("Location: https://github.com/login/oauth/authorize?client_id=" . json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/data/github.json"), true)["id"] . "&redirect_uri=http" . (isSecure() ? "s"  : "") . "://$_SERVER[HTTP_HOST]/admin/callback/&allow_signups=false&scope=read:user");
 die();
